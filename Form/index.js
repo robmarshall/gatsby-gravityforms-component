400b2b90a13b26be8b745df05589e7c162d36b40
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-06-23 16:17:12 +0100

    Pulling in form data, removed props

--- Form/index.js
+++ Form/index.js
@@ -1,30 +1,31 @@
 import React, { Component } from "react"
-import { graphql } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
+import allFormData from "../queries/allForms"
+import FormBuilder from "../FormBuilder"
 
-import Layout from "../components/Layout"
+/**
+ * Component to get a specific Gravity Form form data and
+ * output as a fully functional form.
+ * @param int id    Form ID from Gravity Forms
+ * @param string    lambda API link for Lambda functions when working with
+ *                  netlify or similar
+ */
 
-class GravityFormForm extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return <div />
-  }
+export const GravityFormForm = ({ id, lambda }) => {
+  // Take ID argument, and get Gravity Form data for this form
+  // This is a React Hook, so has to be set here. Cannot go in
+  // a functional component. Thats against the rules.
+  return allFormData(id) && <FormBuilder formData={allFormData(id)} />
 }
 
+export default GravityFormForm
+
 GravityFormForm.defaultProps = {
-  apiUrl: true,
-  slug: true,
-  fields: [],
-  button: true,
+  lambda: "",
 }
 
 GravityFormForm.propTypes = {
-  id: PropTypes.number,
-  slug: PropTypes.bool,
-  fields: PropTypes.array,
-  button: PropTypes.bool,
-  api: PropTypes.bool,
+  id: PropTypes.number.isRequired,
+  lambda: PropTypes.string,
 }
