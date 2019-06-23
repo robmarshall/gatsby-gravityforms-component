1992b27ca03bd2230379ebbb80f1a81246a6285b
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-06-23 17:53:42 +0100

    Working on parsing fields

--- FormBuilder/index.js
+++ FormBuilder/index.js
@@ -5,8 +5,22 @@ import PropTypes from "prop-types"
 class FormBuilder extends Component {
   constructor(props) {
     super(props)
-    console.log(props.formData)
+
+    this.state = {
+      fields: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+
+    // Set value and error state for form fields
+    // Access field by ID
+    props.formData.formFields.forEach(field => {
+      this.state.fields[field.id] = {
+        value: "",
+        error: "",
+      }
+    })
   }
 
   handleSubmit(event) {
@@ -14,8 +28,39 @@ class FormBuilder extends Component {
     const form = event.target
   }
 
+  handleChange(event) {
+    event.preventDefault()
+  }
+
   render() {
-    return <form onSubmit={this.handleSubmit} />
+    const formData = this.props.formData
+
+    const fields = formData.formFields.map(field => {
+      let newInput = [field.id]
+
+      return (
+        <div className={field.cssClass} key={field.id}>
+          <label htmlFor={field.id}>{field.label}</label>
+          <input
+            id={field.id}
+            name={field.id}
+            type="text"
+            value={this.state.fields[field.id].value}
+            onChange={this.handleChange}
+          />
+        </div>
+      )
+    })
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {fields}
+        <input
+          type="submit"
+          value={formData.button.text ? formData.button.text : "Submit"}
+        />
+      </form>
+    )
   }
 }
 
