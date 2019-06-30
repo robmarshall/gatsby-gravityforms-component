b3295a8ae0123d3056e071289081d6c0b3463638
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-06-30 12:08:49 +0100

    Added axios

--- src/utils/passToGravityForms.js
+++ src/utils/passToGravityForms.js
@@ -1,4 +1,6 @@
-export default (url, formData) => {
+const axios = require('axios')
+
+export default (baseUrl, formData) => {
     // Get basic API url
     // Append form ID
     // Create JSON payload
