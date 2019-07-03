bfd6674ffa5449432e3d9d13a369bb3d92bc44bf
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-07-03 11:06:51 +0100

    Changed to .js

---
+++ src/utils/strings.js
@@ -0,0 +1,14 @@
+const strings = {
+    errors: {
+        general:
+            'There was a problem with your submittion. Errors have been highlighted below.',
+        required: 'This field is required.',
+        pattern: 'The format seems to be wrong.',
+        maxChar: {
+            front: 'Need more than',
+            back: 'characters.',
+        },
+    },
+}
+
+export default strings
