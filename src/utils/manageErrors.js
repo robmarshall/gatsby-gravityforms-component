318052f80ec1dd09ec6fbbb000750654efdd0da5
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-07-06 18:00:57 +0100

    Cleaned up loop

--- src/utils/manageErrors.js
+++ src/utils/manageErrors.js
@@ -4,8 +4,7 @@
  */
 
 export const handleGravityFormsValidationErrors = (data, setError) => {
-    const array = Object.keys(data)
-    array.forEach(function(key) {
+    Object.keys(data).forEach(function(key) {
         const id = key.replace('.', '_')
         const fieldId = 'input_' + id
         setError(fieldId, 'gf_validation', data[key])
