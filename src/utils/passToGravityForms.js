32518d99a02818273a755fd2b50d17160714db15
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-07-06 18:01:13 +0100

    Refactored to make cleaner. Only does the sending and getting

--- src/utils/passToGravityForms.js
+++ src/utils/passToGravityForms.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
-import { isObject } from './helpers'
-import { handleGravityFormsValidationErrors } from './manageErrors'
 
-export default async (baseUrl, formData, lambdaEndpoint, setError) => {
+export default async (baseUrl, formData, lambdaEndpoint) => {
     let lambaData = {
         baseUrl: baseUrl,
         payload: formData,
@@ -18,28 +16,17 @@ export default async (baseUrl, formData, lambdaEndpoint, setError) => {
             data: lambaData,
         })
     } catch (err) {
-        const res = err.response
-        // Handle the errors
-        // First check to make sure we have the correct data
-        if (isObject(res.data)) {
-            // Validation errors passed back by Gravity Forms
-            if (res.data.status === 'gravityFormErrors') {
-                // Pass messages to handle that sets react-hook-form errors
-                handleGravityFormsValidationErrors(
-                    res.data.validation_messages,
-                    setError
-                )
-            }
+        // Pass back error
+        return {
+            status: 'error',
+            data: err.response,
         }
-        console.log(err.response)
-
-        // Seemed to be an unknown issue
-        return false
     }
 
-    // Everything went well. Lets add confirmation
-
-    console.log(result)
+    return {
+        status: 'success',
+        data: result,
+    }
 
     return false
 }
