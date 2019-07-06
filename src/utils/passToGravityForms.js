3004e26faae55c0691407eee6f9ebf094cfc17e0
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-07-06 13:37:37 +0100

    Pass form data directly

--- src/utils/passToGravityForms.js
+++ src/utils/passToGravityForms.js
@@ -1,23 +1,10 @@
 import axios from 'axios'
-import { createGfKeyFromField } from './helpers'
 
-export default async (id, baseUrl, formData, lambdaEndpoint) => {
-    console.log(lambdaEndpoint)
-
+export default async (baseUrl, formData, lambdaEndpoint) => {
     let lambaData = {
         baseUrl: baseUrl,
-        payload: {
-            form_id: id,
-        },
+        payload: formData,
     }
-
-    // Set data object from form fields
-    Object.keys(formData).map(function(key) {
-        const data = createGfKeyFromField(key)
-        lambaData['payload'][data] = formData[key]
-    })
-
-    console.log(lambaData)
 
     let result
 
