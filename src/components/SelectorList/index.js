ade2d0a5a605e7e41d0f5fc7792edfb327cdd292
Caleb Pierce <1012241+ridgehkr@users.noreply.github.com>
2021-04-02 10:39:10 -0400

    Remove unique names for checkboxes and radios

--- src/components/SelectorList/index.js
+++ src/components/SelectorList/index.js
@@ -31,7 +31,7 @@ const SelectorList = ({ errors, fieldData, name, register, ...wrapProps }) => {
                                 )}
                                 defaultChecked={isSelected}
                                 id={`${name}_${choiceID}`}
-                                name={`${name}_${choiceID}`}
+                                name={`${name}`}
                                 ref={register({
                                     required:
                                         isRequired && strings.errors.required,
